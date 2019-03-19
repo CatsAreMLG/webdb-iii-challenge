8d54ad4d49820e5bb4faab5d52b7392d31fcb208
CatsAreMLG <joshuadgon@gmail.com>
2019-03-19 14:21:50 -0500

    Change file path for seeds and migrations

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './cohorts.sqlite3'
+      filename: './data/cohorts.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
